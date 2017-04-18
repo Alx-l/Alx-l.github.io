f83fdeaeaee29174bc0a61147aa083594aa2539b
AlexL <lexa.act@gmail.com>
2017-04-18 23:07:19 +0200

    add TextBlock component

---
+++ src/layouts/textBlock/index.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import h from 'react-hyperscript'
+import classNames from 'classnames'
+
+import styles from './textBlock.css'
+
+const TextBlock = (props) => {
+  const className = classNames(styles.root, {
+    [styles.background]: props.background
+  })
+
+  return (
+    h('div', { className }, [
+      props.children
+    ])
+  )
+}
+
+TextBlock.propTypes = {
+  background: React.PropTypes.bool
+}
+
+export default TextBlock
