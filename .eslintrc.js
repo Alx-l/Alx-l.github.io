a2f1b08363b97c15eff33a3141812cc8fe84b5c3
AlexL <lexa.act@gmail.com>
2017-06-03 14:57:26 +0200

    add standardjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,45 +6,12 @@ module.exports = {
     "commonjs": true,
     "es6": true
   },
-  "extends": "eslint:recommended",
+  "extends": "standard",
   "parser": "babel-eslint",
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true
     },
     "sourceType": "module"
-  },
-  "rules": {
-    "arrow-parens": [2, "as-needed", { "requireForBlockBody": true }],
-    "arrow-spacing": [1, { "before": true, "after": true }],
-    "indent": [1, 2,
-      {
-        "SwitchCase": 1,
-        "VariableDeclarator": { "var": 2, "let": 2, "const": 3 }
-      }
-    ],
-    "keyword-spacing": [1, { "before": true, "after": true }],
-    "key-spacing": [1, { "afterColon": true }],
-    "linebreak-style": 0,
-    "no-console": 1,
-    "no-var": 1,
-    "no-trailing-spaces": 1,
-    "no-unexpected-multiline": 1,
-    "no-use-before-define": [2, { "variables": false }],
-    "object-curly-spacing": [1, "always"],
-    "prefer-const": 1,
-    "quotes": [
-      "error",
-      "single",
-      { "allowTemplateLiterals": true }
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "space-before-blocks": [1, "always"],
-    "space-before-function-paren": [1, { "anonymous": "never", "named": "never", "asyncArrow": "always" }],
-    "space-infix-ops": 1,
-    "template-curly-spacing": [1, "always"]
   }
 };
