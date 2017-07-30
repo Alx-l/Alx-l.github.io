536f2bdecc1da0e31123a8883321d32729ef44bb
AlexL <lexa.act@gmail.com>
2017-07-30 19:48:03 +0200

    clean up code

--- src/reusableComponents/tooltip/index.js
+++ src/reusableComponents/tooltip/index.js
@@ -31,7 +31,9 @@ export default class ToolTip extends Component {
   }
 
   handleRAF = () => {
-    if (this.lastScrollYPos !== window.scrollY) { this.setState({ visible: false }) }
+    if (this.lastScrollYPos !== window.scrollY) {
+      this.setState({ visible: false })
+    }
     this.RAF = window.requestAnimationFrame(this.handleRAF)
   }
 
