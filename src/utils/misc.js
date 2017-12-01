67b48f544b419c94b8196bdbfdcca7540188d753
Alex L <lexa.act@gmail.com>
2017-12-01 01:39:54 +0100

    feat: add debounce function

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -34,3 +34,19 @@ export const handleStickyClassOnScroll =
     ? targetNode.classList.add(className)
     : targetNode.classList.remove(className)
 }
+
+export const debounce = (func, wait, immediate) => {
+  let timeout
+  return function() {
+    const context = this
+    const args = arguments
+    const later = function() {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
