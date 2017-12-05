425e73c21de88e2280e8ec0f0b08ef54c962ed8a
Alex L <lexa.act@gmail.com>
2017-12-05 20:12:15 +0100

    feat: make sure we use a legit index, based on how many elements are being transitioned

--- src/reusableComponents/GroupAnimate.js
+++ src/reusableComponents/GroupAnimate.js
@@ -1,6 +1,7 @@
 import h from 'react-hyperscript'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import PropTypes from 'prop-types'
+import lift, { range } from 'space-lift'
 
 const propTypes = {
   list: PropTypes.arrayOf(PropTypes.element).isRequired,
@@ -16,13 +17,24 @@ const propTypes = {
   })
 }
 
+let itemRange
+
 export const GroupAnimate = props => {
   const { list, renderItem, computeKey, classNames = {}, timeout, onEnter, onExit } = props
+  itemRange = []
   return h(TransitionGroup, { className: classNames.root }, list.map((item, i) => {
     return h(CSSTransition, {
       children: renderItem(item),
-      onEnter: onEnter ? el => onEnter(el, i) : undefined,
-      onExit: onExit ? el => onExit(el, i) : undefined,
+      onEnter: onEnter ? el => {
+        if (!itemRange.length) itemRange = lift(itemRange).appendAll(range(list.length).value()).value()
+        const legitIndex = itemRange.shift()
+        return onEnter(el, legitIndex)
+      } : undefined,
+      onExit: onExit ? el => {
+        if (!itemRange.length) itemRange = lift(itemRange).appendAll(range(list.length).value()).value()
+        const legitIndex = itemRange.shift()
+        return onExit(el, legitIndex)
+      } : undefined,
       key: computeKey(item),
       timeout,
       classNames: {
