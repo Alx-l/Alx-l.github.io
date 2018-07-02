7a421c3a2186cc09b4d1e0a76ecf86349285c82e
Alex L <lexa.act@gmail.com>
2018-07-02 13:40:11 +0200

    feat: add service workers

---
+++ serviceworker.js
@@ -0,0 +1,50 @@
+const version = 1
+const staticCacheName = String(version) + 'staticfiles'
+
+addEventListener('install', evt => {
+  skipWaiting()
+  evt.waitUntil(
+    caches.open(staticCacheName).then(staticCache => {
+      // Nice to have
+      staticCache.addAll([
+        'fonts/Tofino-Regular.otf',
+        'images/arrow.svg',
+        'images/avatarpic.jpg'])
+      // Must have
+      return staticCache.addAll(['css/bundle.css', 'js/bundle.js'])
+    })
+  )
+})
+
+addEventListener('fetch', evt => {
+  const request = evt.request
+  evt.respondWith(
+    // First, look in the cache
+    caches.match(request).then(responseFromCache => {
+      if (responseFromCache) {
+        return responseFromCache
+      }
+      // Otherwise fetch from the network
+      return fetch(request)
+    })
+  )
+})
+
+addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName != staticCacheName) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      .then(() => {
+        return clients.claim()
+      })
+  )
+})
