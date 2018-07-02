91927518ae91a37fa8c6caed7e95f79761164991
Alex L <lexa.act@gmail.com>
2018-07-02 17:42:39 +0200

    chore: improve SW config

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const version = 5
+const version = 6
 const staticCacheName = `V${version}-staticFiles`
 
 addEventListener('install', evt => {
@@ -9,22 +9,58 @@ addEventListener('install', evt => {
       staticCache.addAll([
         'fonts/Tofino-Regular.otf',
         'images/arrow.svg',
-        'images/avatarpic.jpg'])
+        'images/avatarpic.jpg'
+      ])
       // Must have
-      return staticCache.addAll(['css/bundle.css', 'js/bundle.js'])
+      return staticCache.addAll([
+        'css/bundle.css',
+        'js/bundle.js',
+        'index.html'
+      ])
     })
   )
 })
 
 addEventListener('fetch', evt => {
   const request = evt.request
+
+  if (request.headers.get('Accept').includes('text/html')) {
+    evt.respondWith(
+      fetch(request).catch(error => {
+        return caches.match('index.html')
+      })
+    )
+    return
+  }
+
+  if (request.headers.get('Accept').includes('image')) {
+    evt.respondWith(
+      caches.match(request).then(responseFromCache => {
+        if (responseFromCache) {
+          return responseFromCache
+        }
+
+        return fetch(request).then(responseFromFetch => {
+          // Put a copy in the cache
+          const copy = responseFromFetch.clone()
+          evt.waitUntil(
+            caches.open(imageCacheName).then(imageCache => {
+              return imageCache.put(request, copy)
+            })
+          )
+          return responseFromFetch
+        })
+      })
+    )
+    return
+  }
+
+  // For every other assets
   evt.respondWith(
-    // First, look in the cache
     caches.match(request).then(responseFromCache => {
       if (responseFromCache) {
         return responseFromCache
       }
-      // Otherwise fetch from the network
       return fetch(request)
     })
   )
