b5c15410ba912bf18808201e861d0556918367ae
Alex L <lexa.act@gmail.com>
2019-04-01 10:11:32 +0200

    new deployment

--- src/settings.js
+++
@@ -1,9 +0,0 @@
-export const settings = {
-  primaryColor: '#2196f3',
-  accentColor: '#6200ea',
-  black: '#333',
-  grey: '#bdbdbd',
-  greyDarken: '#757575',
-  email: 'lexa.act@gmail.com',
-  git: 'https://github.com/alx-l/'
-}
--- src/views/whatido/index.js
+++
@@ -1,6 +0,0 @@
-import h from 'react-hyperscript'
-
-import { Skillz } from './Skillz'
-import { Likes } from './Likes'
-
-export const WhatIDo = () => h('div', [h(Skillz), h(Likes)])
